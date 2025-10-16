abafaf8743e434a88e704833f5c64cf72c060eca
ade-jokes <omotayo.ayeni@pau.edu.ng>
2025-10-16 09:49:58 +0100

    My OS Dev assignment

--- EKEDC Smart Meter.rs
+++ EKEDC Smart Meter.rs
@@ -1,13 +1,23 @@
 use std::io;
 
-fn main(){
+fn main() {
+    let mut input = String::new();
 
-	let mut input = String::new();
+    println!("Enter your energy consumption in kWh:");
+    io::stdin().read_line(&mut input).unwrap();
 
-	println!("Enter your energy consumption in kWh");
-	io::stdin().read_line(&mut input).unwrap();
+    let usage: f64 = input.trim().parse().unwrap();
+    let rate: f64;
 
-	let usage: f64= input.trim().parse().unwrap();
-	let rate: f64;
-	
+    if usage > 200.0 {
+        rate = 30.0;
+    } else if usage > 100.0 {
+        rate = 25.0;
+    } else {
+        rate = 20.0;
+    }
+
+    let total = usage * rate;
+
+    println!("Your total electricity bill is ₦{:.2}", total);
 }
